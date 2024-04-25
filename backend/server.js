bb11cf5b58a9ac11ff169ca6a2d169a1e52984a6
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-04-25 08:48:56 +0800

    first commit

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import { postsRoutes } from './routes/postsRoutes.js';
+import { usersRoutes } from './routes/usersRoutes.js';
+import mongoose from 'mongoose';
+
+
+const app = express();
+
+app.use(express.json()); // middleware that intercepts requests but must be json
+
+app.use('/api/posts', postsRoutes)
+app.use('/api/users', usersRoutes)
+
+mongoose.connect("mongodb://localhost:27017", { dbName: "demo_db" }).then(()=>{
+  console.log("Connected to DB successfully")
+})
+.catch((error) => {console.log("error", error)})
+
+app.listen(4000, 'localhost', () => console.log("Listening to port 4000"))
