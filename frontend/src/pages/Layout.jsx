20f5c172e684bf6e809712c8a4bce530e899b760
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-04-25 09:21:39 +0800

    added navbar

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -3,9 +3,13 @@ import { Link } from "react-router-dom"
 const Layout = () => {
   return (
     <>
-      <header>
-        <nav>
-          <Link to="/" className="fa-solid fa-house"></Link>
+      <header className="bg-indigo-500 text-white">
+        <nav className="flex items-center justify-between p-4">
+          <Link to="/" className="fa-solid fa-house nav-link"></Link>
+          <div className="flex items-center gap-2">
+            <Link to="/login" className="fa-solid fa-right-to-bracket nav-link"></Link>
+            <Link to="/register" className="fa-solid fa-user-plus nav-link"></Link>
+          </div>
         </nav>
       </header>
     </>
