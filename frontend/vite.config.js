52c76d3977f4ca6457755049093cfc8544f89426
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-04-26 02:18:50 +0800

    changed target

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api' : {
-        target: "http://localhost:4000"
+        target: "https://mern-full-crud.onrender.com"
       }
     }
   }
