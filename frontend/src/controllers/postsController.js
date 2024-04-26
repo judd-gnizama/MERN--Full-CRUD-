5b16422003f44a2d198deefdbf6c6c63fcb0ee2d
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-04-26 12:09:09 +0800

    changed /api/posts to ./api/posts

--- frontend/src/controllers/postsController.js
+++ frontend/src/controllers/postsController.js
@@ -1,7 +1,7 @@
 // ------------------------ GET ALL POSTS ---------------------------
 const getPosts = async () => {
   // since method is get, no need to add options
-  const res = await fetch('/api/posts'); 
+  const res = await fetch('./api/posts'); 
   const data = await res.json();
 
   if (!res.ok) {
