9853ef2da79bc874ca465a0e009b7b8677193bbe
judd-gnizama <121470867+judd-gnizama@users.noreply.github.com>
2024-04-28 15:31:49 +0800

    added proxy

--- backend/frontend/vite.config.js
+++ backend/frontend/vite.config.js
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {}
+  server: {
+    proxy: {
+      '/api' : {
+        target: 'http://localhost:4000',
+        changeOrigin: true
+      }
+    }
+  }
 })
 
